99c627784b72d1c40b4303021f767accec8d782b
geranyl <sekelian@gmail.com>
2020-01-29 08:18:33 -0800

    deploy

---
+++ precache-manifest.e1e3db67be392ff87d43473cee6833e6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813e5fdf05ae0e5152079d709a6f949e",
+    "url": "/quiz/audio/bat_transition.mp3"
+  },
+  {
+    "revision": "741286db4215559a28126f6bf4304d2b",
+    "url": "/quiz/audio/correct.mp3"
+  },
+  {
+    "revision": "f8df7c391ff24a566ed3a1dbfa80c6f7",
+    "url": "/quiz/audio/meow_right.mp3"
+  },
+  {
+    "revision": "52a2b13b96ef182175f5cf406f4e0763",
+    "url": "/quiz/audio/meow_sad.mp3"
+  },
+  {
+    "revision": "f398f666bdf24ab0b306b889e48cc954",
+    "url": "/quiz/audio/meow_sad_short.mp3"
+  },
+  {
+    "revision": "b5c93103554a42cbcb77",
+    "url": "/quiz/css/app.e29a93a7.css"
+  },
+  {
+    "revision": "91334f73c285ba638ab8",
+    "url": "/quiz/css/chunk-vendors.34636656.css"
+  },
+  {
+    "revision": "0ef8c09ae75c3820e530f3ea341de5c7",
+    "url": "/quiz/dr-ken-gin-social.png"
+  },
+  {
+    "revision": "83617295d1beb7598941eb3f15b312ea",
+    "url": "/quiz/img/cat-correct.png"
+  },
+  {
+    "revision": "08a4ad8e332f15b3f8c6c7c3a13dcb54",
+    "url": "/quiz/img/cat-incorrect.png"
+  },
+  {
+    "revision": "a8a33e9415e73c1b2964d544c1c79496",
+    "url": "/quiz/img/cat-shelf.a8a33e94.png"
+  },
+  {
+    "revision": "aa42de01f9c3adc57b2d3705efc10ffe",
+    "url": "/quiz/img/dr-ken-gin.aa42de01.png"
+  },
+  {
+    "revision": "bc99a88c7e9298112614e944fa04eec2",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "b5c93103554a42cbcb77",
+    "url": "/quiz/js/app.c8525664.js"
+  },
+  {
+    "revision": "91334f73c285ba638ab8",
+    "url": "/quiz/js/chunk-vendors.c1551bd2.js"
+  },
+  {
+    "revision": "c12c3d7cd7963fb0e93c7f47ccc4291a",
+    "url": "/quiz/manifest.json"
+  },
+  {
+    "revision": "10f52c2aaec46d17d7423e3539814671",
+    "url": "/quiz/questions/quizsamples.csv"
+  },
+  {
+    "revision": "160b4381b3383f23883a4aa34a21faaa",
+    "url": "/quiz/questions/quizshow.csv"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quiz/robots.txt"
+  }
+]);
